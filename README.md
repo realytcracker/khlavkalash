7b074ba10b7a132b3a33fb9630ece0a1861caffe
ytcracker <rapper@gmail.com>
2020-05-12 18:28:13 -0700

    no pizza

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # khlavkalash
+this is an http server that serves only one thing, no matter what you ask for.
+
+it gets its name from the simpsons - https://www.youtube.com/watch?v=885neD-PZEY.
+
+crab juice not included.
+
 no pizza, only khlav kalash.
+
+## installation
+`go get -u https://github.com/realytcracker/khlavkalash/...`
+
+
+## usage
+```
+ __   ___  __   ___  
+|/"| /  ")|/"| /  ") 
+(: |/   / (: |/   /  
+|    __/  |    __/   [khlavkalash] by [ytcracker]
+(// _  \  (// _  \   
+|: | \  \ |: | \  \  
+(__|  \__)(__|  \__) 
+  -f string
+    	path to your khlav kalash
+  -h	prints this helpful garbage
+  -m string
+    	mime type of your khlav kalash (default "image/jpeg")
+  -p uint
+    	port to serve khlav kalash on (80 may require root privs) (default 80)
+  -s string
+    	http server version header (default "nginx/1.17.10")
+  -v	print incoming requests to stdout
+```
---
+++ cmd/khlavkalash/main.go
@@ -0,0 +1,109 @@
+/*
+
+khlavkalash by ytcracker
+use http to serve one thing
+no matter what anyone asks for
+no pizza, only khlav kalash
+
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
+
+var verbose, help bool
+var port uint
+var mimetype, server, filename string
+
+func main() {
+	fmt.Println(" __   ___  __   ___  ")
+	fmt.Println("|/\"| /  \")|/\"| /  \") ")
+	fmt.Println("(: |/   / (: |/   /  ")
+	fmt.Println("|    __/  |    __/   [khlavkalash] by [ytcracker]")
+	fmt.Println("(// _  \\  (// _  \\   ")
+	fmt.Println("|: | \\  \\ |: | \\  \\  ")
+	fmt.Println("(__|  \\__)(__|  \\__) ")
+
+	// program usage and defaults
+	flag.BoolVar(&help, "h", false, "prints this helpful garbage")
+	flag.BoolVar(&verbose, "v", false, "print incoming requests to stdout")
+	flag.UintVar(&port, "p", 80, "port to serve khlav kalash on (80 may require root privs)")
+	flag.StringVar(&mimetype, "m", "image/jpeg", "mime type of your khlav kalash")
+	flag.StringVar(&server, "s", "nginx/1.17.10", "http server version header")
+	flag.StringVar(&filename, "f", "", "path to your khlav kalash")
+
+	flag.Parse()
+
+	if help {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if fileExists(filename) == false {
+		flag.PrintDefaults()
+		fmt.Println("please specify a file to serve with -f.")
+		os.Exit(1)
+	}
+
+	ln, err := net.Listen("tcp", ":"+strconv.FormatUint(uint64(port), 10))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("mime type: " + mimetype)
+	fmt.Println("server version header: " + server)
+	fmt.Println("filename: " + filename)
+	fmt.Println("listening on port " + strconv.FormatUint(uint64(port), 10) + "...")
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		go serveKhlavKalash(conn)
+	}
+}
+
+// serveKhlavKalash serves the khlav kalash
+func serveKhlavKalash(conn net.Conn) {
+	t := time.Now()
+
+	headers := "HTTP/1.1 200 OK\n"
+	headers += "Server: " + server + "\n"
+	headers += "Date: " + t.Format(time.RFC1123) + "\n"
+	headers += "Content-Type: " + mimetype + "\n"
+	headers += "Connection: keep-alive\n\n"
+
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("error:", err.Error())
+	} else if verbose == true {
+		fmt.Println("[" + conn.RemoteAddr().String() + "]: " + string(buf))
+	}
+
+	f, err := ioutil.ReadFile(filename)
+
+	conn.Write([]byte(headers))
+	conn.Write(f)
+	conn.Close()
+}
+
+// fileExists checks if a file exists and is not a directory before we
+// try using it to prevent further errors.
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
